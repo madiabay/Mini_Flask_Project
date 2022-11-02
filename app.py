8a5b4fecb2f193756c30d5d1127b9048555db061
Madi <abaymadi1@gmail.com>
2022-11-02 23:14:57 +0600

    хеширование данных

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session
 from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
 import yaml
 import os
 
@@ -23,6 +24,7 @@ def index():
         name = form['name']
         age = form['age']
         cursor = mysql.connection.cursor()
+        name = generate_password_hash(name)
         cursor.execute("INSERT INTO employee(name, age) VALUES(%s, %s);", (name, age))
         cursor.connection.commit()
         return 'SUCCESS!', 201
@@ -35,7 +37,8 @@ def employees():
     if result_value > 0:
         employees = cursor.fetchall()
         session['username'] = employees[0]['name']
-        return render_template('employee.html', employees_html=employees)
+        return str(check_password_hash(employees[7]['name'], '.amelebalgamspor124:D'))
+        # return render_template('employee.html', employees_html=employees)
     return 'EMPTY'
 
 if __name__ == '__main__':
